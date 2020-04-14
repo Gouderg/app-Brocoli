c9dfcc44dd0b3f4f78b72ea7a2e50dd81c356b79
Gouderg <victor.illien974@gmail.com>
2020-04-14 19:56:31 +0200

    Ajout base de donnée

--- php/constante.php
+++ php/constante.php
@@ -1,5 +1,7 @@
 <?php 
-	//Constantes nécessaires pour la connexion à la base de donnéé
-
-
+	//Constantes nécessaires pour la connexion à la base de donnée
+	define('DB_USER', 'leopoldUSER');
+	define('DB_PASSWORD', 'phpInMyHeart333!!!');
+	define('DB_NAME', 'projetIV_PL');
+	define('DB_SERVER', 'localhost');
 ?>
---
+++ sql/tables.sql
@@ -0,0 +1,61 @@
+#------------------------------------------------------------
+# Table: modele
+#------------------------------------------------------------
+
+#------------------------------------------------------------
+# Change database
+#------------------------------------------------------------
+USE projetIV_PL;
+
+#------------------------------------------------------------
+# Database cleanup
+#------------------------------------------------------------
+DROP TABLE IF EXISTS modele;
+DROP TABLE IF EXISTS type_champ;
+DROP TABLE IF EXISTS champ;
+
+
+CREATE TABLE modele(
+        libelle       Varchar (50) NOT NULL ,
+        nom_fichier   Varchar (50) NOT NULL ,
+        nom_table     Varchar (50) NOT NULL ,
+        date_creation Date NOT NULL
+	,CONSTRAINT modele_PK PRIMARY KEY (libelle)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: type_champ
+#------------------------------------------------------------
+
+CREATE TABLE type_champ(
+        type_champ Varchar (1024) NOT NULL ,
+        actif      Bool NOT NULL
+	,CONSTRAINT type_champ_PK PRIMARY KEY (type_champ)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: champ
+#------------------------------------------------------------
+
+CREATE TABLE champ(
+        id           Int  Auto_increment  NOT NULL ,
+        nom_champ    Varchar (50) NOT NULL ,
+        longueur     Double NOT NULL ,
+        val_min_nb   Double NOT NULL ,
+        val_max_nb   Double NOT NULL ,
+        val_min_date Date NOT NULL ,
+        val_max_date Date NOT NULL ,
+        liste_txt    Varchar (1024) NOT NULL ,
+        fichier      Varchar (1024) NOT NULL ,
+        libelle      Varchar (50) NOT NULL ,
+        type_champ   Varchar (1024) NOT NULL
+	,CONSTRAINT champ_PK PRIMARY KEY (id)
+
+	,CONSTRAINT champ_modele_FK FOREIGN KEY (libelle) REFERENCES modele(libelle)
+	,CONSTRAINT champ_type_champ0_FK FOREIGN KEY (type_champ) REFERENCES type_champ(type_champ)
+)ENGINE=InnoDB;
+
+SET autocommit = 0;
+SET names utf8;
