5d0735331f8890bb88ec9afedf1096c5cab982ca
leo9722 <pralainleopold@gmail.com>
2020-04-22 12:25:37 +0200

    replay v2

--- html/replay.php
+++ html/replay.php
@@ -7,103 +7,123 @@
 <body>
 	<?php require "header.html" ?>
 
-	<div  class="col-md-2"> 
-			<br>
+	<br><br><br><br><br>
+	
+	<form method="post">
+
+		<div class="col-sm-2">
 			<h1>Modèles :</h1>
 			<hr>
-	</div>
-	<div id="rename" >
+		</div>
+
+
+		<div class="row">
+			<div class="col-md-4">
+				<section class="container">
+					<table class="table" id="tabReplay">
+							<!-- Générer par le php -->		
+	  					<thead>
+	    					<tr>
+								<th scope="col">#</th>
+								<th scope="col">Name</th>
+					      			
+					   		</tr>
+						</thead>
+						<tbody>
+					  		<tr>
+					   			<th scope="row">1 </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					    	<tr>
+					      		<th scope="row">2</th>
+					      		<td>php</td>
+					   		 </tr>
+					    	<tr>
+					      		<th scope="row">3</th>
+					     		<td colspan="1">php </td>
+
+	   						</tr>
+	   						<tr>
+					      		<th scope="row">4 </th>
+					      		<td> Completer php </td>
+					     
+					   		</tr>
+					    	<tr>
+					      		<th scope="row">.... </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					    	<tr>
+					      		<th scope="row">... </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					    	<tr>
+					      		<th scope="row">.. </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					    	<tr>
+					      		<th scope="row">.. </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					    	<tr>
+					      		<th scope="row">.. </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+					   		<tr>
+					      		<th scope="row">Last </th>
+					      		<td> Completer php </td>
+					     
+					    	</tr>
+	 					</tbody>
+				</table>
+			</section>
+		</div>
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="form-group">
+						<label for="renommer" class="col-form-label">Renommer le fichier :</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="nomModel" value="Nom du modèle">
+						</div>
+					</div>
+					<br><br>
+				</div>
+
+				<div class="row">
+					<div class="col-auto ">			
+						<label class="police " id="val">Changer la structure :</label>
+
+						<br><br><br><br><br><br>
+					
+						<div class="col-auto">
+							<button type="submit" class="btn btn-outline-primary btn-lg" id="btnGenerer"> <a href="generate.php">Click here </a></button>						
+						</div>
+					</div>
+					<div class="vertical-line col-auto"></div>
+					<div class="col-auto"> 
+						<label class="police " id="val">Changer la valeur :</label>
+						
+						<br><br><br><br><br><br>
+					
+						<div class="col-auto">
+							<button type="submit" class="btn btn-outline-primary btn-lg" id="btnGenerer"> <a href="index.php"> Click here </a></button>						
+						</div>
+					</div>
 		
-				<a class="police"> Renommer le fichier : </a> 
-				<input type="text"  size="50">
-	
-	</div>
-
-	<div class="container">
-
-	<div class="row ">
-		
-		<div class=" row col-md-6">
-		<label class="police " >Changer la valeur :</label>
+				</div>		
+			</div>
 		</div>
-		<span class="vertical-line"></span>
-		<div class=" col-md-6">
-		<label class="police">Changer la structure :</label>
-		</div>
-		<div class="col-md-6">
-		<button type="button" class="btn  btn-lg btn-primary" disabled>Click here</button>
-		</div>
-		<div class="col-md-6">
-		<button type="button" class="btn  btn-lg btn-primary" disabled>Click here</button>
-		</div>
-	</div>
-	</div>
-	<div class="rounded"> 
-			<table class="table table-bordered col-md-3" >
-  				<thead>
-    				<tr>
-						<th scope="col">#</th>
-						<th scope="col">Name</th>
-				      			
-				   	</tr>
-				</thead>
-				<tbody>
-				    <tr>
-				      <th scope="row">1 </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">2</th>
-				      <td>php</td>
-				    </tr>
-				    <tr>
-				      <th scope="row">3</th>
-				      <td colspan="1">php </td>
+	</form>
 
-   					</tr>
-   					<tr>
-				      <th scope="row">4 </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">.... </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">... </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">.. </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">.. </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">.. </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
-				    <tr>
-				      <th scope="row">Last </th>
-				      <td> Completer php </td>
-				     
-				    </tr>
- 				</tbody>
-			</table>
-
-</div>
-
-
+	<br><br><br><br><br>
 
 	<?php require "footer.html" ?>
 </body>
 </html>
+
